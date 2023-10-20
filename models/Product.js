eb38f6961186c4257724f66019407e377e6e2e2b
Fitsum Mehari <m.fitsummehari@gmail.com>
2023-10-20 22:24:00 +0300

    change the keys of the Schema "Product"

--- models/Product.js
+++ models/Product.js
@@ -2,19 +2,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-    name: {
+    pName: {
         type: String,
         // required: true
     },
-    description: {
+    pDesc: {
         type: String,
         // required: true
     },
-    price: {
+    pPrice: {
         type: String,
         // required: true
     },
-    quanity: {
+    pQuantity: {
         type: Number,
         // require: true
     },

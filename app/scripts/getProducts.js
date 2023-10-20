aa9e4b316dacb5dfdfc43ef2f7cc14aa707433a3
Fitsum Mehari <m.fitsummehari@gmail.com>
2023-10-20 22:23:27 +0300

    recieve data from api

---
+++ app/scripts/getProducts.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+    $.ajax({
+        url: "/getProducts",
+        dataType: "json",
+        type: "GET",
+        contentType: "application/json",
+        processData: false,
+        success: function(data, textStatus, jQxhr) {
+            refreshProductsList(data);
+        },
+        error: function(jqxhr, textStatus, errorThrown) {
+            console.log(errorThrown);
+        },
+    });
+});
+
+function refreshProductsList(data) {
+    data.forEach((product) => {
+        var pId = product._id.toString();
+        console.log(pId);
+        const isAvaialble = product.pQuantity > 0 ? "in stock" : "out of stock"
+
+        console.log(product);
+        $("tbody").append(`
+        <tr>
+        <td>${product.pName}</td>
+        <td>${product.pDesc}</td>
+        <td>${product.pPrice}</td>
+        <td>${product.pQuantity}</td>
+        <td>${isAvaialble}</td>
+        <td>
+            <a href="/pages/editProduct.html?id=${product._id}" class="btn btn-warning">Edit</a>
+        </td>
+        <td><button id="${product._id}" class="btn btn-danger">Delete</button></td>
+        </tr>
+        `);
+
+        $(`#${product._id}`).click(function(e) {
+            e.preventDefault();
+            deleteProduct(product._id);
+        });
+    });
+}
+
+function deleteProduct(productid) {
+    var decision = confirm("Are you sure to remove the product?");
+    if (!decision) {
+        return;
+    } else {
+        console.log(productid);
+        $.ajax({
+            url: "/:" + productid,
+            type: "DELETE",
+            success: function(result) {
+                window.location.href = "/pages/home.html";
+            },
+        });
+    }
+}

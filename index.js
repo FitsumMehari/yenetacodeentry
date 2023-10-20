bc696ae74a9196ca1e02b425a20eb7c6273c96c9
Fitsum Mehari <m.fitsummehari@gmail.com>
2023-10-20 22:30:49 +0300

    Change the console.log at the end

--- index.js
+++ index.js
@@ -96,5 +96,5 @@ app.put("/:id", async(req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`);
+    console.log(`App listening on port ${PORT}`);
 });
